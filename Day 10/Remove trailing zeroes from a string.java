d4a8778313bfc5ca0349124a1ee767b62d3c2ef4
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-05-28 08:51:56 +0530

    Create Remove trailing zeroes from a string.java

---
+++ Day 10/Remove trailing zeroes from a string.java
@@ -0,0 +1,22 @@
+/*
+Given a positive integer num represented as a string, return the integer num without trailing zeros as a string. \
+Example 1:
+Input: num = "51230100"
+Output: "512301"
+Explanation: Integer "51230100" has 2 trailing zeros, we remove them and return integer "512301".
+Example 2:
+Input: num = "123"
+Output: "123"
+Explanation: Integer "123" has no trailing zeros, we return integer "123".
+*/
+//code
+class Solution {
+    public String removeTrailingZeros(String num) {
+        //int n=num.length();
+        StringBuilder s=new StringBuilder(num);
+        while(s.length()>0 && s.charAt(s.length()-1)=='0') 
+        s.setLength(s.length()-1);
+        return s.toString();
+        
+    }
+}
