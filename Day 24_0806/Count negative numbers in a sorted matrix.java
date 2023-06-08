9d5f8d3751b344dc27cc5d3797a3db645392cdc6
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-06-08 23:41:00 +0530

    Update Count negative numbers in a sorted matrix.java

--- Day 24_0806/Count negative numbers in a sorted matrix.java
+++ Day 24_0806/Count negative numbers in a sorted matrix.java
@@ -9,3 +9,43 @@ Input: grid = [[3,2],[1,0]]
 Output: 0
 */
 //code
+class Solution {
+    public int countNegatives(int[][] grid) {
+        
+        int r=0,c=grid[0].length-1,count=0;
+        while(r<grid.length && c>=0)
+        {
+            if(grid[r][c]>=0)
+                r++;
+            else
+            {
+                c--;
+                count+=grid.length-r;
+            }
+        }
+        return count;
+        
+        /*
+        int n =grid[0].length,m=grid.length;
+        int count=0;
+        int l=0,r=n-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            for(int i=0;i<m;i++)
+            {
+                if(grid[i][mid]>=0)
+                    l=mid+1;
+                else{
+                    count+=n-mid;
+                    r=mid-1;
+                }
+                    
+            }
+        }
+        return count;
+        */
+        
+    }
+}
+            
