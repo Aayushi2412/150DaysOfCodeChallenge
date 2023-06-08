f218fe1a1afa53e37225c6d4a883d1aac2acd3eb
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-06-08 11:16:23 +0530

    Create Count negative numbers in a sorted matrix.java

---
+++ Day 24_0806/Count negative numbers in a sorted matrix.java
@@ -0,0 +1,11 @@
+/*
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+Example 1:
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+Input: grid = [[3,2],[1,0]]
+Output: 0
+*/
+//code
