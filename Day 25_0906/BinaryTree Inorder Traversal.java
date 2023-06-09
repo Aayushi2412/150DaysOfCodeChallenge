7b62230bb7926a8ee4dd7db257aaaf5c10c06de6
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-06-09 22:22:45 +0530

    Create BinaryTree Inorder Traversal.java

---
+++ Day 25_0906/BinaryTree Inorder Traversal.java
@@ -0,0 +1,83 @@
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
+//code
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*
+//method 1
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+     
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+   */
+import java.util.* ;
+import java.io.*; 
+/*
+    
+    Following is the Binary Tree node structure:
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.data = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.data = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static List < Integer > getInOrderTraversal(TreeNode root) {
+    	// Write your code here.
+        ArrayList<Integer> list=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+        while(true)
+        {
+            if(node!=null)
+            {
+                st.push(node);
+                node=node.left;
+            }
+            else
+            {
+                if(st.isEmpty()) break;
+                node=st.pop();
+                list.add(node.data);
+                node=node.right;
+            }
+        }
+        return list;
+        
+    }
+}
