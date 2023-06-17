939abd1dc7822ffb21d4fe029193dc61a1ebcf9c
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-06-18 00:30:09 +0530

    Update LCA of a binary tree.java

--- Day 33 1706/LCA of a binary tree.java
+++ Day 33 1706/LCA of a binary tree.java
@@ -7,42 +7,23 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public static boolean getPath(TreeNode root, TreeNode n, ArrayList<TreeNode> list){
-        if(root == null){
-            return false;
-        }
-
-        list.add(root);
-
-        if(root.val == n.val){
-            return true;
-        }
-        boolean foundLeft = getPath(root.left,n,list);
-        boolean foundRight = getPath(root.right,n,list);
-
-        if(foundLeft || foundRight){
-            return true;
-
-        }
-        list.remove(list.size()-1);
-        return false;
-       }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> path1 = new ArrayList<>();
-        ArrayList<TreeNode> path2 = new ArrayList<>();
-
-        getPath(root, p, path1);
-        getPath(root, q,path2);
-
-        //lca
-           int i=0;
-           for(; i<path1.size() && i<path2.size();i++){
-               if(path1.get(i) != path2.get(i)){
-                   break;
-               }
-           }
-           TreeNode lca = path1.get(i-1);
-           return lca;
+        if(root==null || root==p || root==q) return root;
+        TreeNode left=lowestCommonAncestor(root.left, p, q);
+        TreeNode right=lowestCommonAncestor(root.right, p,q);
+        if(left==null) return right;
+        else if(right==null) return left;
+        else return root; 
     }
 }
+
