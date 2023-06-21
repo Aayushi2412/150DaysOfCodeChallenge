3ab1342f34b7f3730d003ffeb1a95635594b9186
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-06-21 23:09:24 +0530

    Create BST Iterator.java

---
+++ Day 37 21_06/BST Iterator.java
@@ -0,0 +1,54 @@
+/*
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+  */
+//code
+**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    List<Integer> list;
+    int next=0;
+    public BSTIterator(TreeNode root) {
+        this.list=new ArrayList<>();
+        dfs(root);
+    }
+    
+    public int next() {
+        return list.get(next++);
+    }
+    
+    public boolean hasNext() {
+        return list.size()>next;
+    }
+    public void dfs(TreeNode root)
+    {
+        if(root==null) return;
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
