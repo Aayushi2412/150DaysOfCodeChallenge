e2d1f64ad18f5ac53847526b0fac9b32a0947f26
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-07-03 20:44:38 +0530

    Create Finding peak element.java

---
+++ Day 49 03-07/Finding peak element.java
@@ -0,0 +1,49 @@
+/*
+A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+You must write an algorithm that runs in O(log n) time.
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+Constraints:
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i.
+*/
+//code
+class Solution {
+    public int findPeakElement(int[] nums) {
+        //linear search
+        //tc=O(n), sc=o(1)
+        // int n=nums.length;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i]>nums[i+1]))
+        //     return i;
+        // }
+        // return 0;
+
+        //Binary Search
+        //tc=O()
+        int n=nums.length;
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int l=1, r=n-2;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]>nums[mid-1]) l=mid+1;
+            else  r=mid-1;                //if(nums[mid]>nums[mid+1])
+        }
+        return -1;
+
+    }
+}
