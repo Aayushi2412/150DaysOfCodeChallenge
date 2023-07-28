008dd17b53c67e5849dca4f53feb2415f9f91575
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-07-28 16:20:26 +0530

    Update Replace elements with greatest element on right side.java

--- Day 74 28-07/Replace elements with greatest element on right side.java
+++ Day 74 28-07/Replace elements with greatest element on right side.java
@@ -1,1 +1,16 @@
-
+//arr=[17,18,5,4,6,1] 
+//output=[18,6,6,6,1,-1]
+//code
+class Solution {
+    public int[] replaceElements(int[] arr) {
+       int n=arr.length;
+       int[] ans=new int[n];
+       int max=-1;
+       for(int i=n-1;i>=0;i--)
+       {
+           ans[i]=max;
+           max=Math.max(max, arr[i]);
+       }
+       return ans;
+    }
+}
