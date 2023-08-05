58c7e22485673d27cd69f09cc7a453efe539d48c
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-08-05 22:49:22 +0530

    Update Unique BST.java

--- Day 82 05-08/Unique BST.java
+++ Day 82 05-08/Unique BST.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int numTrees(int n) {
+        //1 to n, construct a bst out of the sequence     1,2,3,4,5,6,...
+        int[] g=new int[n+1];
+        g[0]=g[1]=1;
 
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                g[i]+=g[j-1]*g[i-j];
+            }
+        }
+        return g[n];
+    }
+}
