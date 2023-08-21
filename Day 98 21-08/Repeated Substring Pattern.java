32e2163d31897f87cb172fc0248482c4143ac7ff
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-08-21 23:54:15 +0530

    Update Repeated Substring Pattern.java

--- Day 98 21-08/Repeated Substring Pattern.java
+++ Day 98 21-08/Repeated Substring Pattern.java
@@ -15,3 +15,22 @@ Constraints:
 1 <= s.length <= 104
 s consists of lowercase English letters.
   */
+//code
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int i=n/2;i>=1;i--)
+        {
+            if(n%i==0)
+            {
+                int m=n/i;
+                String sub=s.substring(0,i);
+                StringBuilder sb=new StringBuilder();
+                for(int j=0;j<m;j++)
+                    sb.append(sub);
+                if(sb.toString().equals(s)) return true;
+            }
+        }
+        return false;
+    }
+}
