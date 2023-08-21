0412d57e1e0ce62ec2f7d099ca15ffa4f59b42ea
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-08-21 23:54:36 +0530

    Update Repeated Substring Pattern.java

--- Day 98 21-08/Repeated Substring Pattern.java
+++ Day 98 21-08/Repeated Substring Pattern.java
@@ -1,6 +1,6 @@
 /*
 Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
-Example 1:
+Example 1: 
 Input: s = "abab"
 Output: true
 Explanation: It is the substring "ab" twice.
