b16ef52b4f65ea2ab3efe071cc9cb905e63d39b0
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-08-23 09:53:14 +0530

    Update Reorganize String.java

--- Day 100 23-08/Reorganize String.java
+++ Day 100 23-08/Reorganize String.java
@@ -1,1 +1,8 @@
-
+// Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+// Return any possible rearrangement of s or return "" if not possible.
+// Example 1:
+// Input: s = "aab"
+// Output: "aba"
+// Example 2:
+// Input: s = "aaab"
+// Output: ""
