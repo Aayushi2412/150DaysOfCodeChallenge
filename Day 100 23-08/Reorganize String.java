9f9f950569cdff54a5532ce777ab77cf2fa5e785
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-08-23 11:10:47 +0530

    Update Reorganize String.java

--- Day 100 23-08/Reorganize String.java
+++ Day 100 23-08/Reorganize String.java
@@ -6,3 +6,44 @@
 // Example 2:
 // Input: s = "aaab"
 // Output: ""
+//code
+class Sol
+  {
+    public String reorganiseString(String s)
+    {
+      int n=s.length();
+      int[] freq=new int[26];
+      for(int i=0;i<n;i++)
+        freq[s.charAt(i)-'a']++;
+      int max=0, letter=0;
+      for(int i=0;i<26;i++)
+        {
+          if(freq[i]>max)
+          {
+            max=freq[i];
+            letter=i;
+          }
+        }
+      if(max> (n+1)/2) return "";
+      int indx=0;
+      char[] ans=new char[n];
+      while(freq[letter]>0)         //even indices
+        {
+            ans[indx]=(char)(letter+'a');
+            indx+=2;
+            freq[letter]--;
+        }
+      for(int i=0;i<26;i++)
+        {
+          while(freq[i]>0)
+            {  
+              if(indx>ans.length)indx=1;
+              ans[indx]=(char)(i+'a');
+              indx+=2;
+              freq[i]--;
+            }
+        }
+      return String.valueOf(ans);
+    }
+  }
+          
