a1abe0e4b07b02cede2c757b3cddd0f0032a38f5
Aayushi <106343054+Aayushi2412@users.noreply.github.com>
2023-09-01 23:32:50 +0530

    Update Number of Subarrays With GCD Equal to K.java

--- Day 108 01-09/Number of Subarrays With GCD Equal to K.java
+++ Day 108 01-09/Number of Subarrays With GCD Equal to K.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int subarrayGCD(int[] nums, int k) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int curGcd=nums[i];
+            if(curGcd==k) ans++;
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]<k) break;
+                curGcd=gcd(nums[j], curGcd);
+                if(curGcd==k) ans++;
+            }
+        }
+        return ans;
+    
+    }
+    private int gcd(int a, int b)
+    {
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+}
